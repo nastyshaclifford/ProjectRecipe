ae169f0046f73213b674b697d982c1b3c8b1750e
Olya <olyakorzhenevich@gmail.com>
2025-04-24 10:29:41 +0300

    add header

--- js/favorites.js
+++ js/favorites.js
@@ -1,4 +1,16 @@
 document.addEventListener('DOMContentLoaded', function() {
+    fetch("./components/header.html")
+    .then((response) => response.text())
+    .then((data) => {
+      document.getElementById("header").innerHTML = data;
+    });
+
+  // Подключение footer
+  fetch("./components/footer.html")
+    .then((response) => response.text())
+    .then((data) => {
+      document.getElementById("footer").innerHTML = data;
+    });
     const favorites = JSON.parse(localStorage.getItem('favorites')) || [];
     const favoritesList = document.getElementById('favoritesList');
 
